c190dd2f11af6beebf88ab209e1ae9eb7fc88c3b
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-18 12:12:27 -0500

    method.md

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
